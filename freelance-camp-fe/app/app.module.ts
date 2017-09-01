e762fb612a57b5ea3e2dcb5bffe093d6a511108c
Sam Holst <sawohol@gmail.com>
2017-09-01 16:00:12 -0600

    first commit

---
+++ freelance-camp-fe/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomepageComponent } from './homepage.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    HomepageComponent
+  ],
+  bootstrap: [
+    HomepageComponent
+  ]
+})
+export class AppModule {}
---
+++ freelance-camp-fe/app/homepage.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  template: '<h1>Freelance Bootcamp Homepage'
+})
+export class HomepageComponent {}
