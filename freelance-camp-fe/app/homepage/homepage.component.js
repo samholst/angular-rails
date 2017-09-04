edf20f00e8f26c5f95e4643fcfa9276ca4af6a59
Sam Holst <sawohol@gmail.com>
2017-09-04 12:41:25 -0600

    added view and organized files

--- freelance-camp-fe/app/homepage/homepage.component.js
+++ freelance-camp-fe/app/homepage/homepage.component.js
@@ -12,8 +12,9 @@ var HomepageComponent = /** @class */ (function () {
     }
     HomepageComponent = __decorate([
         core_1.Component({
-            selector: 'main-app',
-            template: '<h1>Sam\'s Angular App',
+            moduleId: module.id,
+            selector: 'homepage',
+            templateUrl: 'homepage.component.html',
         })
     ], HomepageComponent);
     return HomepageComponent;
