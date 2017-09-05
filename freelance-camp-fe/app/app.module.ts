6c385d57432b0bae9738cb513c982aee177ebc6d
Sam Holst <sawohol@gmail.com>
2017-09-04 22:16:08 -0600

    added proposal class and view

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AppComponent } from './app.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 @NgModule({
   imports: [
@@ -17,7 +18,8 @@ import { ProposalListComponent } from './proposal/proposal-list.component';
     AppComponent,
     HomepageComponent,
     DocumentsComponent,
-    ProposalListComponent
+    ProposalListComponent,
+    ProposalNewComponent
   ],
   bootstrap: [
     AppComponent
--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(1, 'ABC Company', 'https://www.samwholst.com', 'Ruby on Rails', 150, 120, 15, 'sam@samwholst.com')
+  proposalTwo: Proposal = new Proposal(2, 'Coo Company', 'https://www.samwholst.com', 'Ruby on Rails', 150, 120, 15, 'sam@samwholst.com')
+  proposalThree: Proposal = new Proposal(3, 'Pwn\'d Co', 'https://www.samwholst.com', 'Ruby on Rails', 150, 120, 15, 'sam@samwholst.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
