59a1ebcaac448a538896023cb172192f04fab836
Sam Holst <sawohol@gmail.com>
2017-09-04 22:32:01 -0600

    added proposal form

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,7 +13,8 @@ import { ProposalNewComponent } from './proposal/proposal-new.component';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
